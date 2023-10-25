85f8a6bfd54b7e54a46782d9f80f317a5e222cdf
LuizHenrique2802 <141173511+LuizHenrique2802@users.noreply.github.com>
2023-10-25 13:06:50 -0300

    Add files via upload

---
+++ exemplo 2.2.py
@@ -0,0 +1,18 @@
+def soma (x, y):
+    return x + y
+
+print (soma (3, 5))
+
+print (soma(103, 5))
+print (soma(23, 5))
+print (soma(35,5))
+
+def simples (cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    
+def medio (cor):
+    if cor == 'marrom':
+          return 'esolheu certo'
+        
+            
